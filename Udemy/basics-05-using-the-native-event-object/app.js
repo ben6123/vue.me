6d6925aca371c1259eb163bf0393a49a433ca964
thbo <ewiben6000@gmail.com>
2022-11-21 13:50:41 +0100

    events

--- Udemy/basics-05-using-the-native-event-object/app.js
+++ Udemy/basics-05-using-the-native-event-object/app.js
@@ -2,10 +2,55 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      // fullname:'',
     };
   },
+  watch: {
+    counter(value) { //anytime counter value changes this fxn is executed
+      // if (value > 50) {
+      //   this.counter=0;
+      // }
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        },2000)
+          
+      }
+    }
+    // name(value) {//anytime the name value is changed this fxn executes
+    //   if (value==='') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {//anytime the name value is changed this fxn executes
+    //   if (value==='') {
+    //     this.fullname = '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value; 
+    //   }
+    // },
+  },
+  computed: {
+    fullname() {//very similar to watchers but can be more usefull in a case of tracking more than one input.
+      // console.log('heyyyyyyyyyyy')
+      if (this.name === "" && this.lastName === '') {
+        return " ";
+      }
+      return this.name + " " + this.lastName;
+    }
+  },
   methods: {
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Arnold';
+    },
     setName(event) {
       this.name = event.target.value;
     },
@@ -18,6 +63,7 @@ const app = Vue.createApp({
     },
     resetInput(e) {
       this.name = '';
+      this.lastName = '';
     }
   }
 });
