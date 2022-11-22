3e280254647e1a50e20707f66ee0abcfe38727f4
thbo <ewiben6000@gmail.com>
2022-11-22 17:23:50 +0100

    first commit

---
+++ Udemy/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<section>
+<h2>My friends</h2>
+<ul>
+  <li>hey</li>
+</ul>
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manual",
+          name: "Manuel Lorenzo",
+          phone: "01234 5678 991",
+          email: " manuel@localhost.com",
+        },
+        {
+          id: "juli",
+          name: "Julie Jones",
+          phone: "01234 888 991",
+          email: " manuel123@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Udemy/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friends.name }}</h2>
+    <button>Show Details</button>
+    <ul>
+      <li><strong>Phone:</strong>{{ friends.phone }}</li>
+      <li><strong>Email:</strong>{{ friends.email }}</li>
+      <li></li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manual",
+        name: "Manuel Lorenzo",
+        phone: "01234 5678 991",
+        email: " manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toogleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
