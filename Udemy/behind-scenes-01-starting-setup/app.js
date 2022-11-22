fe2600b0b0672eba53dfa6c4b840b2fd6ad3efb8
thbo <ewiben6000@gmail.com>
2022-11-22 13:59:08 +0100

    explanations

--- Udemy/behind-scenes-01-starting-setup/app.js
+++ Udemy/behind-scenes-01-starting-setup/app.js
@@ -10,12 +10,43 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
     },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()')
+  },
+  created() {
+    console.log('created()')
+  },
+  beforeMount() {
+    console.log('beforeMount()')
+  },
+  mounted() {
+    console.log('mounted()')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()')
+  },
+  updated() {
+    console.log('updated()')
+  },
+  beforeUnmount() {
+   console.log('beforeUnmount()');
+  },
+  unmounted() {
+   console.log('unmounted()');
   },
 });
 
 app.mount('#app');
+
+// setTimeout(() => {
+//   app.unmount()
+// }, 3000);
+
+
 const app2 = Vue.createApp({
   data() {
     return {
