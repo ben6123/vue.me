0ab19fbd0d80e51896b2bca470a7be1362b2aa1a
thbo <ewiben6000@gmail.com>
2022-11-28 13:59:23 +0100

    added some dynamic styling

--- Udemy/cmp-adv-01-starting-setup/src/App.vue
+++ Udemy/cmp-adv-01-starting-setup/src/App.vue
@@ -1,25 +1,58 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <!-- <TheHeader/> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent==='active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent==='manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
+
+    <!--  <badge-list></badge-list>
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    ></UserInfo>
+    <CourseGoal #default="slotProps">
+      
+        <h2>{{slotProps.item}}</h2>
+        <p>{{slotProps['anotherProp']}}</p>
+    </CourseGoal> -->
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoal from "./components/CourseGoal.vue";
+import ActiveGoals from "./components/ActiveGoal.vue";
+import ManageGoals from "./components/ManageGoal.vue";
+
 export default {
+  components: {
+    "the-header": TheHeader,
+    // UserInfo,
+    // BadgeList,
+    // CourseGoal,
+    ActiveGoals,
+    ManageGoals,
+  },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
