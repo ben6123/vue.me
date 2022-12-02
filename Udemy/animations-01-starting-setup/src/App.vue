27c69ca22321b474964c938f98788f2dbcaf9c94
thbo <ewiben6000@gmail.com>
2022-12-02 23:24:05 +0100

    animations

--- Udemy/animations-01-starting-setup/src/App.vue
+++ Udemy/animations-01-starting-setup/src/App.vue
@@ -1,23 +1,104 @@
 <template>
+  <router-view v-slot="slotProps">
+    <transition name="fade" mode="out-in">
+<component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+  <!-- <div class="container">
+    <ListData></ListData>
+  </div>
   <div class="container">
-    <div class="block" :class="{animate:animatedBlock}"></div>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition :css="false" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+      @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled">
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="tooglePara">Toogle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade" mode="out-in">
+      <button @click="showUsers" v-if="!userIsVisible">Show users</button>
+      <button @click="hideUsers" v-else>Hide users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
-  </div>
+  </div> -->
 </template>  
 
 <script>
+// import ListData from './components/ListData.vue';
+
 export default {
+  components: {
+    // ListData,
+  },
   data() {
-    return { dialogIsVisible: false ,animatedBlock:false};
+    return { dialogIsVisible: false, animatedBlock: false, paraIsVisible: false, userIsVisible: false, enterInterval: null, leaveInterval: null };
   },
   methods: {
+    enterCancelled() {
+      clearInterval(this.enterInterval);
+      // done()
+    },
+    leaveCancelled() {
+      clearInterval(this.leaveInterval);
+      // done()
+    },
+    beforeEnter(el) {
+      console.log("before enter");
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log("enter");
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter() {
+      console.log("afterEnter");
+    },
+    beforeLeave() {
+      console.log("before Leave");
+    },
+    leave(el, done) {
+      console.log("leave");
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave() {
+      console.log("afterLeave");
+    },
+    showUsers() {
+      this.userIsVisible = true;
+    },
+    hideUsers() {
+      this.userIsVisible = false;
+    },
+    tooglePara() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
     animateBlock() {
       this.animatedBlock = true;
     },
@@ -35,12 +116,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 button {
   font: inherit;
   padding: 0.5rem 2rem;
@@ -50,11 +134,13 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #a80b48;
   border-color: #a80b48;
 }
+
 .block {
   width: 8rem;
   height: 8rem;
@@ -62,6 +148,7 @@ button:active {
   margin-bottom: 2rem;
   /* transition: transform 1s ease-out; */
 }
+
 .container {
   max-width: 40rem;
   margin: 2rem auto;
@@ -73,21 +160,56 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
-.animate{
+
+.animate {
   animation: slide 0.5s ease-out forwards;
-  background-color:pink;
+  background-color: pink;
   /* transform: translateX(-150px); */
 }
 
+
+
 @keyframes slide {
-  0%{
-    transform:translateX(0) scale(1);
+  0% {
+    transform: translateX(0) scale(1);
   }
-  50%{
+
+  50% {
     transform: translateX(-120px) scale(1.1);
   }
-  100%{
-    transform: translateX(-150px) scale(1);
+
+  100% {
+    transform: translateX(-150) scale(1);
   }
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.route-enter-from {}
+
+.route-enter-active {
+  animation: slide 0.4s ease-in;
+}
+
+.route-enter-to {}
+
+.route-enter-active {
+  animation: slide 0.4s ease-out;
 }
 </style>
---
+++ Udemy/animations-01-starting-setup/src/components/ListData.vue
@@ -0,0 +1,64 @@
+<template>
+    <TransitionGroup tag="ul" name="user">
+      <li v-for="user in users" :key="user" @click="removeUser(user)"><button>{{ user }}</button></li>
+    </TransitionGroup>
+  
+  <div>
+    <input type="text" name="" id="" ref="userInput">
+    <button @click="addUser()">Add user</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      users: ['max', 'arrom', 'jude', 'chris', 'patra'],
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.unshift(this.$refs.userInput.value)
+    },
+    removeUser(user) {
+      this.users = this.users.filter(usr => usr !== user)
+    },
+
+  }
+}
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+
+.user-enter-from{
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.user-enter-active{
+  transition: all 0.5s ease-out;
+}
+.user-enter-to,.user-leave-from{
+  opacity: 1;
+  transform: translateX(0);
+}
+.user-leave-active{
+  transition: all 0.5s ease-in;
+  position: absolute;
+}
+.user-leave-to{
+  opacity: 0;
+  transform: translateX(30px);
+}
+.user-move{
+  transition: transform 0.5s ease-in;
+}
+</style>
