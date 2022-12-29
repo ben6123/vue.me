7dc6fd9c9522649cf6f2745e532feedfb80fbfbd
thbo <ewiben6000@gmail.com>
2022-12-29 14:55:34 +0100

    migrating to composition API

--- Udemy/composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
+++ Udemy/composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
@@ -14,48 +14,86 @@
 
 <script>
 import ProjectItem from './ProjectItem.vue';
+import { ref, computed,watch ,toRefs} from "vue";
 
 export default {
   components: {
     ProjectItem,
   },
-  props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
+  props: ['user'], 
+  // props: { 'user': String },
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+    // console.log(props)
+
+    const hasProjects = computed(() => props.user.projects && availableProjects.value.length > 0);
+    const availableProjects = computed(function () {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter((prj) =>
+          prj.title.includes(activeSearchTerm.value)
         );
       }
-      return this.user.projects;
-    },
-  },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
+      return props.user.projects;
+    })
+
+   function updateSearch(val) {
+      enteredSearchTerm.value = val
+    }
+
+    watch(enteredSearchTerm, (val) => {
       setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
+        if (val === enteredSearchTerm.value) {
+          activeSearchTerm.value = val;
         }
       }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
+    });
+
+    // const propsWithRefs = toRefs(props)
+    // const user=propsWithRefs.user
+// if you want to watch one prop
+    const {user}=toRefs(props)
+    watch(user,()=>{enteredSearchTerm.value=''})
+
+    return { enteredSearchTerm, hasProjects, availableProjects, updateSearch }
   },
+  // props: ['user'],
+  // data() {
+  //   return {
+  //     enteredSearchTerm: '',
+  //     activeSearchTerm: '',
+  //   };
+  // },
+  // computed: {
+  // hasProjects() {
+  //   return this.user.projects && this.availableProjects.length > 0;
+  // },
+  //   availableProjects() {
+  //     if (this.activeSearchTerm) {
+  //       return this.user.projects.filter((prj) =>
+  //         prj.title.includes(this.activeSearchTerm)
+  //       );
+  //     }
+  //     return this.user.projects;
+  //   },
+  // },
+  // methods: {
+  //   updateSearch(val) {
+  //     this.enteredSearchTerm = val;
+  //   },
+  // },
+  // watch: {
+  //   enteredSearchTerm(val) {
+  //     setTimeout(() => {
+  //       if (val === this.enteredSearchTerm) {
+  //         this.activeSearchTerm = val;
+  //       }
+  //     }, 300);
+  //   },
+  //   user() {
+  //     this.enteredSearchTerm = '';
+  //   },
+  // },
 };
 </script>
 
